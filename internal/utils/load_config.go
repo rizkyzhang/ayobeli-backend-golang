ef6ada2ab037e7817e2a26ff836a1a6d85dae82c
rizkyzhang <rizkyzhangdev@gmail.com>
2023-05-27 21:24:44 +0700

    feat(utils): add load config

---
+++ internal/utils/load_config.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"log"
+
+	"github.com/rizkyzhang/ayobeli-backend/domain"
+	"github.com/spf13/viper"
+)
+
+func LoadConfig(path string) *domain.Env {
+	env := domain.Env{}
+	viper.SetConfigFile(path)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(&env)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	if env.AppEnv == "development" {
+		log.Println("The App is running in development env")
+	}
+
+	return &env
+}

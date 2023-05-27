37f7372598abf34539696d4b6b12f525d65d7504
rizkyzhang <rizkyzhangdev@gmail.com>
2023-05-27 21:27:00 +0700

    feat(bootstrap): add db

---
+++ boostrap/db.go
@@ -0,0 +1,29 @@
+package bootstrap
+
+import (
+	"context"
+	"log"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/rizkyzhang/ayobeli-backend/domain"
+)
+
+func NewPostgresDB(env *domain.Env) *sqlx.DB {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dbUrl := env.DBUrl
+	db, err := sqlx.ConnectContext(ctx, "pgx", dbUrl)
+	if err != nil {
+		log.Fatalf("Can't connect to Postgres DB with error %s", err)
+	}
+
+	return db
+}
+
+func ClosePostgresDBConnection(db *sqlx.DB) {
+	db.Close()
+	log.Println("Connection to Postgres DB closed")
+}

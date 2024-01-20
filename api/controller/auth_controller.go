85480f78f3353de3a394f3786549da150cbcd925
rizkyzhang <rizkyzhangdev@gmail.com>
2024-01-20 15:49:35 +0700

    feat(controller/auth): add GetAccessToken method

--- api/controller/auth_controller.go
+++ api/controller/auth_controller.go
@@ -47,3 +47,28 @@ func (b *baseAuthController) SignUp(c echo.Context) error {
 
 	return response_util.FromCreated().WithEcho(c)
 }
+
+func (b *baseAuthController) GetAccessToken(c echo.Context) error {
+	email := c.Request().Header.Get("email")
+	password := c.Request().Header.Get("password")
+
+	err := b.validate.Var(email, "email")
+	if err != nil {
+		return response_util.FromBadRequestError(errors.New("invalid email")).WithEcho(c)
+	}
+	err = b.validate.Var(password, "min=8")
+	if err != nil {
+		return response_util.FromBadRequestError(errors.New("min password length is 8")).WithEcho(c)
+	}
+
+	accessToken, err := b.authUsecase.GetAccessToken(email, password)
+	if err != nil {
+		if err.Error() == "user not found" {
+			return response_util.FromNotFoundError(err).WithEcho(c)
+		}
+
+		return response_util.FromError(err).WithEcho(c)
+	}
+
+	return response_util.FromData(accessToken).WithEcho(c)
+}

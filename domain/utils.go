abae5e36858ca4a696b189cbcec4ebf2e109e3ab
rizkyzhang <rizkyzhangdev@gmail.com>
2024-01-20 15:34:01 +0700

    feat(domain): add FirebaseVerifyPasswordURL to Env struct

--- domain/utils.go
+++ domain/utils.go
@@ -7,20 +7,21 @@ import (
 )
 
 type Env struct {
-	AppEnv                 string `mapstructure:"APP_ENV"`
-	ServerAddress          string `mapstructure:"SERVER_ADDRESS"`
-	FirebaseCredentialPath string `mapstructure:"FIREBASE_CREDENTIAL_PATH"`
-	ContextTimeout         int    `mapstructure:"CONTEXT_TIMEOUT"`
-	TestDBUrl              string `mapstructure:"TEST_DB_URL"`
-	TestDBUser             string `mapstructure:"TEST_DB_USER"`
-	TestDBPassword         string `mapstructure:"TEST_DB_PASSWORD"`
-	DBUrl                  string `mapstructure:"DB_URL"`
-	DBName                 string `mapstructure:"DB_NAME"`
-	AesSecret              string `mapstructure:"AES_SECRET"`
-	AccessTokenExpiryHour  int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
-	RefreshTokenExpiryHour int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
-	AccessTokenSecret      string `mapstructure:"ACCESS_TOKEN_SECRET"`
-	RefreshTokenSecret     string `mapstructure:"REFRESH_TOKEN_SECRET"`
+	AppEnv                    string `mapstructure:"APP_ENV"`
+	ServerAddress             string `mapstructure:"SERVER_ADDRESS"`
+	FirebaseCredentialPath    string `mapstructure:"FIREBASE_CREDENTIAL_PATH"`
+	FirebaseVerifyPasswordURL string `mapstructure:"FIREBASE_VERIFY_PASSWORD_URL"`
+	ContextTimeout            int    `mapstructure:"CONTEXT_TIMEOUT"`
+	TestDBUrl                 string `mapstructure:"TEST_DB_URL"`
+	TestDBUser                string `mapstructure:"TEST_DB_USER"`
+	TestDBPassword            string `mapstructure:"TEST_DB_PASSWORD"`
+	DBUrl                     string `mapstructure:"DB_URL"`
+	DBName                    string `mapstructure:"DB_NAME"`
+	AesSecret                 string `mapstructure:"AES_SECRET"`
+	AccessTokenExpiryHour     int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
+	RefreshTokenExpiryHour    int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
+	AccessTokenSecret         string `mapstructure:"ACCESS_TOKEN_SECRET"`
+	RefreshTokenSecret        string `mapstructure:"REFRESH_TOKEN_SECRET"`
 }
 
 type AesEncryptUtil interface {

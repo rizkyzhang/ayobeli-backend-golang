31a3dccac0400b0ed63e77cda772f157311fa90d
rizkyzhang <rizkyzhangdev@gmail.com>
2024-01-27 19:17:33 +0700

    feat(main): use logger util

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	route "github.com/rizkyzhang/ayobeli-backend-golang/api/route"
 	"github.com/rizkyzhang/ayobeli-backend-golang/bootstrap"
 	docs "github.com/rizkyzhang/ayobeli-backend-golang/docs"
-	"github.com/sirupsen/logrus"
+	"github.com/rizkyzhang/ayobeli-backend-golang/internal/utils"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
@@ -29,37 +29,18 @@ func main() {
 	app := bootstrap.App()
 	env := app.Env
 	db := app.DB
-	firebaseAuth := app.FirebaseAuth
 	defer app.CloseDBConnection()
-
+	firebaseAuth := app.FirebaseAuth
+	logger := utils.NewLoggerUtil(env)
 	docs.SwaggerInfo.Host = env.Host
-
-	if env.AppEnv != "prod" {
-		logrus.SetFormatter(&logrus.TextFormatter{})
-	} else {
-		logrus.SetFormatter(&logrus.JSONFormatter{})
-	}
 
 	e := echo.New()
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogURI:    true,
-		LogError:  true,
-		LogMethod: true,
-		LogStatus: true,
-		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
-			logData := logrus.WithFields(logrus.Fields{
-				"URI":    values.URI,
-				"method": values.Method,
-				"status": values.Status,
-			})
-			if values.Status >= 300 {
-				logData.Errorf("failed request with status %d", values.Status)
-			} else {
-				logData.Infof("success request with status %d", values.Status)
-			}
-
-			return nil
-		},
+		LogURI:        true,
+		LogError:      true,
+		LogMethod:     true,
+		LogStatus:     true,
+		LogValuesFunc: logger.EchoMiddlewareFunc(),
 	}))
 
 	route.Setup(env, db, firebaseAuth, e)
